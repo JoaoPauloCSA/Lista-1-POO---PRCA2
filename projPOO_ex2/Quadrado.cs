f9833c6f0e49f90e1ff8c10f4de4454f01058cda
JoaoPauloCSA <jp10alencar@gmail.com>
2024-10-03 19:38:57 -0300

    Add files via upload

---
+++ projPOO_ex2/Quadrado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projPOO_ex2
+{
+    internal class Quadrado
+    {
+        private int a;
+        private int b;
+        private int area;
+        public void setLado(int a)
+        {
+            this.a = a;
+        }
+        public void SetLado(int b)
+    }
+}
